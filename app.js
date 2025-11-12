82fd6900a96477d3e980557afac887a7f5188cc3
Repo <alextin2609@gmail.com>
2025-11-12 11:31:52 -0600

    no se

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ const pool = mysql.createPool({
     user: process.env.MYSQL_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.MYSQL_PORT,        
+    connectTimeout: 30000,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
